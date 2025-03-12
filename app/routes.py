193eb4cce889469f645c371da7a4e0224491b1e6
Christian Bonilla <christianurielbonillasuarez@gmail.com>
2025-03-11 23:56:40 -0700

    Final Report

--- app/routes.py
+++ app/routes.py
@@ -33,7 +33,72 @@ def get_single_task(pk):
     response = requests.get(url)
     if response.status_code == 200:
         single_task = response.json().get("task")
-        return render_template("detail.hml", task=single_task)
+        return render_template("detail.html", task=single_task)
+    return (
+        render_template("error.html", error=response.status_code),
+        response.status_code
+    )
+
+# Update
+@app.get("/tasks/<int:pk>/edit/")
+def get_edit_form(pk):
+    url = "%s/%s" % (BACKEND_URL, pk)
+    response = requests.get(url)
+    if response.status_code == 200:
+        single_task = response.json().get("task")
+        return render_template("edit.html", task=single_task)
+    return(
+        render_template("error.html", error=response.status_code),
+        response.status_code
+    )
+
+@app.post("/tasks/<int:pk>/edit/")
+def edit_task(pk):
+    url = "%s/%s" % (BACKEND_URL, pk)
+    task_data = flask_request.form
+    response = requests.put(url, json=task_data)
+    if response.status_code == 204:
+        return render_template("success.html", message="Task edited")
+    return (
+        render_template("error.html", error=response.status_code),
+        response.status_code
+    )
+
+# Delete
+@app.get("/tasks/<int:pk>/delete/")
+def get_delete_form(pk):
+    url = "%s/%s" % (BACKEND_URL, pk)
+    response = requests.get(url)
+    if response.status_code == 200:
+        single_task = response.json().get("task")
+        return render_template("delete.html", task=single_task)
+    return(
+        render_template("error.html", error=response.status_code),
+        response.status_code
+    )
+
+@app.post("/tasks/<int:pk>/delete/")
+def delete_task(pk):
+    url = "%s/%s" % (BACKEND_URL, pk)
+    response = requests.delete(url)
+    if response.status_code == 204:
+        return render_template("success.html", message="Task deleted")
+    return (
+        render_template("error.html", error=response.status_code),
+        response.status_code
+    )
+
+# Create
+@app.get("/tasks/create/")
+def get_create_form():
+    return render_template("create.html")
+    
+@app.post("/tasks/create/")
+def create_task():
+    task_data = flask_request.form
+    response = requests.post(BACKEND_URL, json=task_data)
+    if response.status_code == 204:
+        return render_template("success.html", message="Task created")
     return (
         render_template("error.html", error=response.status_code),
         response.status_code
